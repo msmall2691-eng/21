9928f63b6776eaec79beb06aed0ca0fb4eba0d43
Megan S <msmall2691@gmail.com>
2026-04-15 08:21:03 -0400

    fix(invoice): add SmsManagerModule import to resolve DI

--- packages/twenty-server/src/modules/invoice/invoice.module.ts
+++ packages/twenty-server/src/modules/invoice/invoice.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { SmsManagerModule } from 'src/modules/messaging/sms-manager/sms-manager.module';
+
 import { InvoiceService } from './services/invoice.service';
 import { InvoiceNotificationService } from './services/invoice-notification.service';
 import { SendInvoiceEmailJob } from './jobs/send-invoice-email.job';
 import { SendInvoiceSmsJob } from './jobs/send-invoice-sms.job';
 
 @Module({
+  imports: [SmsManagerModule],
   providers: [
     InvoiceService,
     InvoiceNotificationService,
