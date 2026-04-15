800a85190f9d93013a0f8abe0bdf5ef354fd2034
Megan S <msmall2691@gmail.com>
2026-04-15 16:09:01 -0400

    fix: remove InvoiceNotificationService to fix MESSAGE_QUEUE_undefined crash
    Removed InvoiceNotificationService due to missing MessageQueue reference in Twenty v1.21.

--- packages/twenty-server/src/modules/invoice/invoice.module.ts
+++ packages/twenty-server/src/modules/invoice/invoice.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 
 import { InvoiceService } from './services/invoice.service';
-import { InvoiceNotificationService } from './services/invoice-notification.service';
 import { SendInvoiceEmailJob } from './jobs/send-invoice-email.job';
 import { SendInvoiceSmsJob } from './jobs/send-invoice-sms.job';
+
+// InvoiceNotificationService removed - uses MessageQueue.generalQueue which
+// does not exist in Twenty v1.21 (causes MESSAGE_QUEUE_undefined crash)
+// Re-add once the queue reference is fixed to use a valid MessageQueue enum value
 
 @Module({
   providers: [
     InvoiceService,
-    InvoiceNotificationService,
     SendInvoiceEmailJob,
     SendInvoiceSmsJob,
   ],
-  exports: [InvoiceService, InvoiceNotificationService],
+  exports: [InvoiceService],
 })
 export class InvoiceModule {}
